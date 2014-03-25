257a512248eb7ba08c158b99588ff44dcb27345b
Hans Hörberg <hans.horberg@umu.se>
2014-03-25 16:42:02 +0100

    Added documentation.

--- doc/conf.py
+++ doc/conf.py
@@ -14,8 +14,6 @@
 
 import sys
 import os
-import sphinx_bootstrap_theme
-#pip install sphinx_bootstrap_theme
 
 import alabaster
 #https://github.com/bitprophet/alabaster
@@ -108,7 +106,6 @@ pygments_style = 'sphinx'
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #html_theme = 'default'
-#html_style = 'dirg-base.css'
 html_theme_path = [alabaster.get_path()]
 html_theme = 'alabaster'
 html_sidebars = {
@@ -128,76 +125,6 @@ html_theme_options = {
    'github_banner': True,
 
 }
-bgcolor = 'black'
-
-#html_theme = 'bootstrap'
-#html_theme_path = sphinx_bootstrap_theme.get_html_theme_path()
-
-
-# Theme options are theme-specific and customize the look and feel of a
-# theme further.
-"""
-html_theme_options = {
-    # Navigation bar title. (Default: ``project`` value)
-    'navbar_title': "",
-
-    # Tab name for entire site. (Default: "Site")
-    'navbar_site_name': "IdProxy",
-
-    # A list of tuples containing pages or urls to link to.
-    # Valid tuples should be in the following forms:
-    #    (name, page)                 # a link to a page
-    #    (name, "/aa/bb", 1)          # a link to an arbitrary relative url
-    #    (name, "http://example.com", True) # arbitrary absolute url
-    # Note the "1" or "True" value above as the third argument to indicate
-    # an arbitrary url.
-    'navbar_links': [
-        #("Examples", "examples"),
-        #("Link", "http://example.com", True),
-    ],
-
-    # Render the next and previous page links in navbar. (Default: true)
-    'navbar_sidebarrel': True,
-
-    # Render the current pages TOC in the navbar. (Default: true)
-    'navbar_pagenav': True,
-
-    # Global TOC depth for "site" navbar tab. (Default: 1)
-    # Switching to -1 shows all levels.
-    'globaltoc_depth': 2,
-
-    # Include hidden TOCs in Site navbar?
-    #
-    # Note: If this is "false", you cannot have mixed ``:hidden:`` and
-    # non-hidden ``toctree`` directives in the same page, or else the build
-    # will break.
-    #
-    # Values: "true" (default) or "false"
-    'globaltoc_includehidden': "true",
-
-    # HTML navbar class (Default: "navbar") to attach to <div> element.
-    # For black navbar, do "navbar navbar-inverse"
-    'navbar_class': "navbar",
-
-    # Fix navigation bar to top of page?
-    # Values: "true" (default) or "false"
-    'navbar_fixed_top': "true",
-
-    # Location of link to source.
-    # Options are "nav" (default), "footer" or anything else to exclude.
-    'source_link_position': "nav",
-
-    # Bootswatch (http://bootswatch.com/) theme.
-    #
-    # Options are nothing with "" (default) or the name of a valid theme
-    # such as "amelia" or "cosmo".
-    'bootswatch_theme': "",
-
-    # Choose Bootstrap version.
-    # Values: "3" (default) or "2" (in quotes)
-    'bootstrap_version': "3",
-}
-"""
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
